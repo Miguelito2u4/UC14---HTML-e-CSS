1853bf813cca8f0f475e537a191102fec1b59921
Miguelito2u4 <127671791+Miguelito2u4@users.noreply.github.com>
2026-07-24 18:55:26 -0300

    Add files via upload

---
+++ UC15-JS/script.js
@@ -0,0 +1,14 @@
+alert("OLÁÁÁÁÁÁÁÁÁÁÁÁÁÁ seja bem vindo")
+let nome = prompt ("Digite seu nome de usuario")
+console.log ("Seu nome de usuario É " + nome)
+let cidade = prompt ("De onde VC é")
+console.log ("TU VEIO DE " + cidade)
+let numero = prompt ("digite o numero misterious")
+
+if (numero == 7) {
+    alert("acertasse boy")
+} else {
+    alert ("errouuuuuuuuuuuuuuuuuuuuuuuuuuuuu")
+}
+
+console.log ("ele tentou esse numero")
