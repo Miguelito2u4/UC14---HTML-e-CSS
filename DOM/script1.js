a9d8f264309ca21217fbd14ab2987c1774fdb228
Miguelito2u4 <127671791+Miguelito2u4@users.noreply.github.com>
2026-09-17 17:44:54 -0300

    Add files via upload

---
+++ DOM/script1.js
@@ -0,0 +1,8 @@
+function mudarTexto() {
+    document.getElementById("titulo").
+    textContent = "voce vai ser pego viu!";
+}
+function mudarcor() {
+    document.getElementById("mensagem").
+    style.color = "brown";
+}
